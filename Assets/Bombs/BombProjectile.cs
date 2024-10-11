dc3b2a0369abe1d01eba50f080d9d60f3db2e458
haweom <100crocki@gmail.com>
2024-10-11 15:59:19 +0200

    Add basic bomb functionality
    -throwing mechanic
    -first force push tests

---
+++ Assets/Bombs/BombProjectile.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+
+public class BombProjectile : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    private Collider2D[] _explosionCollider = null;
+
+    [SerializeField] private float explosionForceMultiplier = 5f;
+    [SerializeField] private float explosionRadius = 5f;
+    [SerializeField] private float speed = 2.5f;
+    [SerializeField] private float timeAlive = 3f;
+    public float direction;
+    public float throwStrength;
+
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _rb.velocity = new Vector2(speed * throwStrength * direction, 0);
+    }
+
+    private void Update()
+    {
+        timeAlive -= Time.deltaTime;
+        if (timeAlive <= 0)
+        {
+            Debug.Log("Boom");
+            Explode();
+            Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    void Explode()
+    {
+        _explosionCollider = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+
+        foreach (Collider2D o in _explosionCollider)
+        {
+            Rigidbody2D orb = o.GetComponent<Rigidbody2D>();
+            if (orb != null)
+            {
+                Vector2 distanceV = o.transform.position - transform.position;
+                if (distanceV.magnitude > 0)
+                {
+                    float distance = distanceV.magnitude < 0.5 ? 0.5f : distanceV.magnitude;
+                    float explosionForce = explosionForceMultiplier / distance;
+                    orb.AddForce(distanceV.normalized * explosionForce);
+                    /*orb.velocity = new Vector2(orb.velocity.x + (distanceV.x * explosionForce),
+                        orb.velocity.y + (distanceV.y * explosionForce));*/
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
---
+++ Assets/Bombs/BombThrow.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombThrow : MonoBehaviour
+{
+    [SerializeField] private GameObject bomb;
+    [SerializeField] private Transform bombGun;
+    private GameObject _thrown;
+    private float _strength;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (_thrown == null)
+            {
+                Throw();
+            }
+        }
+    }
+
+    private void Throw()
+    {
+        _thrown = Instantiate(bomb, bombGun.position, Quaternion.identity);
+
+        bool throwDirection = GetThrowDirection();
+        BombProjectile script = _thrown.GetComponent<BombProjectile>();
+        if (throwDirection)
+        {
+            script.direction = 1;
+            
+        }
+        else
+        {
+            script.direction = -1;
+        }
+        script.throwStrength = _strength;
+    }
+
+    private bool GetThrowDirection()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        _strength = Math.Abs(mousePosition.x - bombGun.position.x);
+        _strength = _strength > 5 ? 1 : _strength/5;
+        Debug.Log(_strength);
+        if (mousePosition.x - bombGun.position.x < 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
