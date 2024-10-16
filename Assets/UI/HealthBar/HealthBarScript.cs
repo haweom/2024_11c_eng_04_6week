8a12bffe1a695dcae064ea9845ad42a868d6e588
s27064 <s27064@pjwstk.edu.pl>
2024-10-16 12:17:41 +0200

    Add healthbar animation

--- Assets/UI/HealthBar/HealthBarScript.cs
+++ Assets/UI/HealthBar/HealthBarScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,22 @@ using UnityEngine.UI;
 public class HealthBarScript : MonoBehaviour
 {
     public Slider healthSlider;
+    [SerializeField] private Slider easeHealthSlider;
+    
+    [SerializeField] private float easeSpeed = 0.05f;
+
+    private void Update()
+    {
+        if (easeHealthSlider.value != healthSlider.value)
+        {
+            easeHealthSlider.value = Mathf.Lerp(easeHealthSlider.value, healthSlider.value, easeSpeed);
+        }
+    }
 
     public void SetHealth(float health)
     {
         healthSlider.value = health;
+        //easeHealthSlider.value = Mathf.Lerp(easeHealthSlider.value, healthSlider.value, easeSpeed);
     }
     
 }
