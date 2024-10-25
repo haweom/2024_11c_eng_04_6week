600015a4c441c9b2fa917ce6e28c7faeeb3f1a5e
EfeEnderer <eenderer94@gmail.com>
2024-10-25 20:55:14 +0300

    Barrels

--- Assets/BarrelRespawn.cs
+++ Assets/BarrelRespawn.cs
@@ -3,11 +3,22 @@ using UnityEngine;
 public class BarrelSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject barrelPrefab;
-    [SerializeField] private float spawnInterval = 5f; 
+    [SerializeField] private float spawnInterval = 5f;
+    [SerializeField] private float spawnRange = 10f;
+    private Transform _playerTransform;
 
     private void Start()
     {
-        InvokeRepeating("SpawnBarrel", 0f, spawnInterval);
+        _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        InvokeRepeating("CheckAndSpawnBarrel", 0f, spawnInterval);
+    }
+
+    private void CheckAndSpawnBarrel()
+    {
+        if (_playerTransform != null && Vector2.Distance(transform.position, _playerTransform.position) <= spawnRange)
+        {
+            SpawnBarrel();
+        }
     }
 
     public void SpawnBarrel()
---
+++ Assets/RollingBarrel.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class RollingBarrel : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 250f;
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float damage = 50f;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward * -rotationSpeed * Time.deltaTime);
+
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            IDamageable damageable = other.GetComponent<IDamageable>();
+            if (damageable != null)
+            {             
+                damageable.Damage(damage);
+            }
+            Destroy(gameObject);
+        }
+
+      
+    }
+}
