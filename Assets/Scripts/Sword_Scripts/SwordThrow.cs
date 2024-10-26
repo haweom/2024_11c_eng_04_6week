8a32ea0f48dc3845dae099a3870d6d2375894560
s27064 <s27064@pjwstk.edu.pl>
2024-10-26 20:53:41 +0200

    Add sword now returns to player after 5 seconds

--- Assets/Scripts/Sword_Scripts/SwordThrow.cs
+++ Assets/Scripts/Sword_Scripts/SwordThrow.cs
@@ -9,9 +9,12 @@ public class SwordThrow : MonoBehaviour
     [SerializeField] private float damageAmount;
     [SerializeField] private CircleCollider2D swordCollider;
     [SerializeField] private float throwDetectionRadius = 1.0f;
+    [SerializeField] private float timeBeforeSwordReturning;
 
+    private float returnSpeed = 10f;
     private Rigidbody2D _swordRb;
     private Transform _parentTransform;
+    private Transform _player;
     private bool _isFlying;
     private bool _isReturning;
     
@@ -22,6 +25,8 @@ public class SwordThrow : MonoBehaviour
         _isFlying = true;
         _swordRb = GetComponentInParent<Rigidbody2D>();
         _parentTransform = transform.parent;
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(ReturnTimer());
     }
 
     private void Update()
@@ -31,11 +36,15 @@ public class SwordThrow : MonoBehaviour
             RotateSword();
             
             DetectEnemiesInRange();
-        }
-        else
+        } 
+        else if (_isReturning)
         {
-            _isFlying = false;
+            ReturnToPlayer();
         }
+        // else
+        // {
+        //     _isFlying = false;
+        // }
     }
     
     private void RotateSword()
@@ -63,13 +72,33 @@ public class SwordThrow : MonoBehaviour
             }
         }
     }
+
+    private void ReturnToPlayer()
+    {
+        Vector2 directionToPlayer = (_player.position - transform.position).normalized;
+        
+        _swordRb.velocity = directionToPlayer * returnSpeed;
+        
+        float angle = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg;
+        _parentTransform.rotation = Quaternion.Euler(0, 0, angle);
+        
+        if (Vector2.Distance(transform.position, _player.position) < 0.5f)
+        {
+            _swordRb.velocity = Vector2.zero;
+        }
+    }
     
     private IEnumerator ReturnTimer()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(timeBeforeSwordReturning);
         
         _isFlying = false;
         _isReturning = true;
+    }
+    
+    public bool IsReturning()
+    {
+        return _isReturning;
     }
 
     public void SetIsFlying(bool isFlying)
--- Assets/Scripts/Sword_Scripts/pickUpSword.cs
+++ Assets/Scripts/Sword_Scripts/pickUpSword.cs
@@ -8,16 +8,18 @@ public class pickUpSword : MonoBehaviour
 {
     private PlayerAttack _playerAttack;
     private Rigidbody2D _rb2d;
+    private SwordThrow _swordThrow;
 
     private void Start()
     {
         _playerAttack = FindObjectOfType<PlayerAttack>();
         _rb2d = GetComponentInParent<Rigidbody2D>();
+        _swordThrow = FindObjectOfType<SwordThrow>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && _rb2d.velocity is { x: 0, y: 0 })
+        if (other.CompareTag("Player") && _swordThrow.IsReturning())
         {
             _playerAttack.SetHasSword(true);
             Destroy(transform.parent.gameObject);
