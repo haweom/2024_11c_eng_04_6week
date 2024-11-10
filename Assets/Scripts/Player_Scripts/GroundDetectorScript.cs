1642584bead929d9e1557e91229508e99e74e307
s27064 <s27064@pjwstk.edu.pl>
2024-11-10 12:40:51 +0100

    Refactor unnecesary code

--- Assets/Scripts/Player_Scripts/GroundDetectorScript.cs
+++ Assets/Scripts/Player_Scripts/GroundDetectorScript.cs
@@ -6,8 +6,6 @@ public class GroundDetectorScript : MonoBehaviour
     [SerializeField] private float groundCheckRadius = 0.1f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform gizmosDraw;
-    [SerializeField] private float timeBetweenJumps = 0.1f;
-    private float _currentTime;
     private Transform groundCheckPosition;
     private bool _isGrounded;
 
@@ -29,10 +27,7 @@ public class GroundDetectorScript : MonoBehaviour
     private void CheckGround()
     {
         Collider2D collider = Physics2D.OverlapCircle(groundCheckPosition.position, groundCheckRadius, groundLayer);
-        if (_currentTime > timeBetweenJumps)
         _isGrounded = collider;
-        
-        _currentTime += Time.deltaTime;
     }
     
     private void OnDrawGizmosSelected()
